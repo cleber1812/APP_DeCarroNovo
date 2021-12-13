4d8625a15ac658a660b3c6853a20d21f3cc78e8b
Cleber1812 <cleber1812roberto@gmail.com>
2021-12-13 07:25:41 -0300

    Teste 7

--- service/api.js
+++ service/api.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 // const api = axios.create({baseURL: '127.0.0.1:3000/'}) 
 // const api = axios.create({baseURL: 'https://api-carros-aula.herokuapp.com/'}) 
 // const api = axios.create({baseURL: 'http://192.168.3.118:3000/'})
+//
 
 const api = axios.create({baseURL: 'https://desolate-wildwood-71856.herokuapp.com/'})
 
