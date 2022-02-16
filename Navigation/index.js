c8cbabbcdbcfd1aaf101cd9f275c1dee03b9c4fe
Cleber1812 <cleber1812roberto@gmail.com>
2022-02-16 08:58:47 -0300

    Navigation com botão logout

--- Navigation/index.js
+++ Navigation/index.js
@@ -11,20 +11,54 @@ import Procurar from '../Pages/Procurar';
 import Login from '../Pages/Login';
 import CadastrarPessoa from '../Pages/CadastrarPessoa';
 
+import { useAuth } from '../hooks/AuthState';
+import { TouchableOpacity } from 'react-native';
+import Icones from 'react-native-vector-icons/Ionicons';
+
+const IconSaida = () => <Icones name="enter" size={40} color="#FF00FF"/>;
 
 const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
+
+  const {signOut} = useAuth()
+
+  const handleLogout = async(navigation) => {
+    try{
+      await signOut()
+      console.log(navigation)
+      navigation.navigate('Login')
+    }
+    catch(e){}
+
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Stack.Navigator> */}
+      <Stack.Navigator initialRouteName='Login'>      
         <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
         <Stack.Screen name="Procurar" component={Procurar} options={{headerShown:false}}/>
         <Stack.Screen name="Anunciar" component={Anunciar} options={{headerShown:false}}/>
         <Stack.Screen name="AnunciarCarro" component={AnunciarCarro} options={{headerShown:false}}/>
         <Stack.Screen name="AtualizarAnuncio" component={AtualizarAnuncio} options={{headerShown:false}}/>
         <Stack.Screen name="DeletarAnuncio" component={DeletarAnuncio} options={{headerShown:false}}/>
-        <Stack.Screen name="Logout" component={Logout} options={{headerShown:false}}/>
+        {/* <Stack.Screen name="Logout" component={Logout} options={{headerShown:false}}/> */}
+        <Stack.Screen name="Logout" component={Logout} options={({navigation})=>({
+          headerLeft: ()=> null,
+          headerRight: ()=> (
+            <TouchableOpacity
+              onPress={()=> handleLogout(navigation)}              
+            >
+              <IconSaida/>
+
+            </TouchableOpacity>
+
+          )
+
+        })}/>
+
+
         <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
         <Stack.Screen name="Cadastrar" component={CadastrarPessoa} options={{headerShown:false}}/>
       </Stack.Navigator>
